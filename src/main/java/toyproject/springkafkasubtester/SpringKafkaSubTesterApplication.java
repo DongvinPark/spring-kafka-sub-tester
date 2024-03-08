13a1027fa3167942eced82a4022bdb56d14705ea
DongvinPark <dongvin99@naver.com>
2024-03-08 16:26:09 +0900

    initial commit

---
+++ src/main/java/toyproject/springkafkasubtester/SpringKafkaSubTesterApplication.java
@@ -0,0 +1,13 @@
+package toyproject.springkafkasubtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSubTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaSubTesterApplication.class, args);
+	}
+
+}
